a1af858d5d9057d9afd00f988e984eb4105c8f32
Sterling Camden <sterling@relateiq.com>
2016-10-22 15:46:36 -0700

    mouse actions and typin

--- src/SiqStoryTellerCtrl.ts
+++ src/SiqStoryTellerCtrl.ts
@@ -3,13 +3,42 @@ import {SiqStoryNode} from 'siqstory-journalist/.src/types';
 
 let nodeIdToTextNode = {};
 
+class ClickBubble {
+    downtime: number;
+    element: HTMLElement;
+    constructor() {
+        var self = this;
+        self.downtime = Date.now();
+        self.element = document.createElement('div');
+        self.element.classList.add('click-bubble');
+        setTimeout(function() {
+            self.element.classList.add('down');
+        });
+    }
+
+    up() {
+        var self = this;
+        setTimeout(function() {
+            self.element.classList.remove('down');
+            if (self.element.parentNode) {
+                self.element.parentNode.removeChild(self.element);
+            }
+        }, Math.max(100 - (Date.now() - this.downtime), 0))
+    }
+}
+
 export class SiqStoryTellerCtrl {
-    iframe: HTMLIFrameElement;
-    storyIndex: number = 0;
-    story: SiqStoryTwist[];
+    private iframe: HTMLIFrameElement;
+    private element: HTMLElement;
+    private pointer: HTMLElement;
+    private storyIndex: number = 0;
+    private story: SiqStoryTwist[];
+    private currentClickBubble: ClickBubble;
     constructor($element, LibrarySrvc) {
         var self = this;
         self.iframe = $element.find('iframe')[0];
+        self.element = $element.find('.story-teller')[0];
+        self.pointer = $element.find('.story-teller-mouse-cursor')[0];
         // clear it out cause it's easier to go from the root
         self.iframe.contentDocument.removeChild(self.iframe.contentDocument.childNodes[0]);
         LibrarySrvc.getLatestStory().then(function(story: SiqStoryTwist[]) {
@@ -26,7 +55,7 @@ export class SiqStoryTellerCtrl {
             return;
         }
         let lastTwist = self.story[self.storyIndex - 1];
-        let nextFrameDelay = thisTwist.timeSincePageLoad - (lastTwist && lastTwist.timeSincePageLoad || 0);
+        let nextFrameDelay = Math.ceil(thisTwist.timeSincePageLoad - (lastTwist && lastTwist.timeSincePageLoad || 0));
         setTimeout(function() {
             let twist = thisTwist;
             let targetNode = self.findNodeByNodeId(twist.targetNode);
@@ -63,6 +92,44 @@ export class SiqStoryTellerCtrl {
                 case 'attributes':
                     if (targetNode instanceof Element) {
                         targetNode.setAttribute(twist.attributeName, twist.attributeValue);
+                    }
+                    break;
+                case 'resize':
+                    self.element.style.width = (self.iframe.width = twist.width.toString()) + 'px';
+                    self.element.style.height = (self.iframe.height = twist.height.toString()) + 'px';
+                    break;
+                case 'event':
+                    switch (twist.eventType) {
+                        case 'mousemove':
+                            var top = twist.clientY + 'px';
+                            var left = twist.clientX + 'px';
+                            self.pointer.style.top = top;
+                            self.pointer.style.left = left;
+                            if (self.currentClickBubble) {
+                                self.currentClickBubble.element.style.top = top;
+                                self.currentClickBubble.element.style.left = left;
+                            }
+
+                            break;
+                        case 'mousedown':
+                            self.pointer.classList.add('mousedown');
+                            var top = twist.clientY + 'px';
+                            var left = twist.clientX + 'px';
+                            self.currentClickBubble = new ClickBubble();
+                            self.currentClickBubble.element.style.top = top;
+                            self.currentClickBubble.element.style.left = left;
+                            self.element.appendChild(self.currentClickBubble.element);
+                            break;
+                        case 'mouseup':
+                            self.pointer.classList.remove('mousedown');
+                            self.currentClickBubble.up();
+                            self.currentClickBubble = null;
+                            break;
+                        case 'input':
+                            if (targetNode instanceof HTMLInputElement || targetNode instanceof HTMLTextAreaElement) {
+                                targetNode.value = twist.textValue;
+                            }
+                            break;
                     }
                     break;
             }
